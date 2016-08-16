f47d9c35bf5d5ffde8cf638f1114dc31445cdcc6
DESKTOP-APUMJV7\libedi <libedi@gmail.com>
2016-08-16 14:30:55 +0900

    @PreAuthorize 설정

--- my-spring-security/src/main/java/com/libedi/myproject/controller/HomeController.java
+++ my-spring-security/src/main/java/com/libedi/myproject/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.libedi.myproject.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.libedi.myproject.domain.Account;
 
 /**
  * Home Controller
@@ -18,5 +22,31 @@ public class HomeController {
 	@RequestMapping(value = "/")
 	public String index(){
 		return "index";
+	}
+	
+	/**
+	 * ADMIN 권한 또는 /getPrivateMessage?userId= 가 로그인 아이디와 같을 경우
+	 * @param account
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/getPrivateMessage")
+	@PreAuthorize("(#account.userId == principal.Username) or hasAuthority('ADMIN')")
+	public String authstring(Account account, Model model){
+		model.addAttribute("msg", "당신은 관리자이거나 요청 파라미터랑 아이디가 같습니다.");
+		return "authorizedMessage";
+	}
+	
+	/**
+	 * USER 권한인 경우
+	 * @param account
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/getUserMessage")
+	@PreAuthorize("hasAuthority('USER')")
+	public String userMessage(Account account, Model model){
+		model.addAttribute("msg", "당신은 유저권한입니다.");
+		return "authorizedMessage";
 	}
 }
---
+++ my-spring-security/src/main/webapp/WEB-INF/views/authorizedMessage.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Authorized Message Page</title>
+</head>
+<body>
+메시지 : ${msg}
+</body>
+</html>
